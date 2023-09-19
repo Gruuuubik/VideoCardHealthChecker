a4745d6f195e4ba670f981708acb9fa109e70fe3
Pavel Zhuravlevich <p.zhuravlevich@gmail.com>
2023-09-19 20:33:16 +0200

    Add project files.

---
+++ VideoCardHealthChecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Management;
+using System.Runtime.Versioning;
+
+namespace VideoCardHealthChecker
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			if (OperatingSystem.IsWindows())
+			{
+				ShowVideoCards();
+			}
+
+			Console.ReadKey();
+		}
+
+		[SupportedOSPlatform("windows")]
+		static void ShowVideoCards()
+		{
+			var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
+
+			foreach (var queryObj in searcher.Get())
+			{
+				Console.WriteLine(queryObj["VideoProcessor"]);
+			}
+		}
+	}
+}
